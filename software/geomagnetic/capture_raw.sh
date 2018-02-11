d8c7ec30f84076b45add32d0f5601c1543aabf99
bkuschak <bkuschak@yahoo.com>
2018-02-11 21:43:06 +0000

    Added systemd service to run capture script from boot, and restart as needed.

--- software/geomagnetic/capture_raw.sh
+++ software/geomagnetic/capture_raw.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-
+#
 # Run the capture script in background at a higher priority.  Need to be root to adjust priority, 
 # but don't run the capture script as root.
+
 USER=$(whoami)
-sudo -b nice -n -5 sudo -b -u $USER sh -c 'nohup python capture_raw.py -p4 -l32768 -P2 -q > /dev/null 2>&1  &'
+sudo -b nice -n -5 sudo -b -u $USER sh -c 'nohup python pyppm_capture_raw.py -p4 -l32768 -P2 -q > /dev/null 2>&1  &'
 
